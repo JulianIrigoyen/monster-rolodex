4d35432de01ae921bb4dc1ebeeb765ae4d7570fc
Julian Martin Irigoyen <julian.irigoyen@joinmosaic.com>
2020-02-11 08:51:09 -0300

    Added search box

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ import './card-list.style.css'
 export const CardList = props => (
     <div className='card-list'>
         {props.monsters.map(monster => (
-            <Card monster={monster}/>
+            <Card key = {monster.id} monster={monster}/>
           ))}
     </div>
 );
