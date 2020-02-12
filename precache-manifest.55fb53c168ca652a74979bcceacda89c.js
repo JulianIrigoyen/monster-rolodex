404beffd9a497acd06659beef8f9e92166b7c321
Julian Martin Irigoyen <julian.irigoyen@joinmosaic.com>
2020-02-12 19:34:53 -0300

    Updates

---
+++ precache-manifest.55fb53c168ca652a74979bcceacda89c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605ba39d93d4ed5615651a68f7f7e1c7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "545fc2a784ada1d058f9",
+    "url": "/monster-rolodex/static/css/main.fcc2293b.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545fc2a784ada1d058f9",
+    "url": "/monster-rolodex/static/js/main.d4126a33.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
